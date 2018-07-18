841164796f76933567069cd9bb3cdd4410a4f2ad
Mark Lawrence <lawrence.ml@gmail.com>
2018-07-17 22:50:37 -0400

    Stubbed out CLI interface

--- lib/adopt_a_dog.rb
+++ lib/adopt_a_dog.rb
@@ -1,5 +1,6 @@
-require "adopt_a_dog/version"
+require_relative "adopt_a_dog/version"
+require_relative "./adopt_a_dog/cli"
 
-module AdoptADog
-  # Your code goes here...
-end
+# module AdoptADog
+#   # Your code goes here...
+# end
