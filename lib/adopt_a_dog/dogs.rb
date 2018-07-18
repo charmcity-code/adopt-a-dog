eb73b4f93573a71fe98bd3c9577b147cba728be5
Mark Lawrence <lawrence.ml@gmail.com>
2018-07-18 01:49:01 -0400

    tested new instances of stubbed adoptable dogs in bin/console

--- lib/adopt_a_dog/dogs.rb
+++ lib/adopt_a_dog/dogs.rb
@@ -3,13 +3,24 @@ class AdoptADog::Dogs
 
   def self.adoptable
     #should return instances of adoptable dogs
-    puts "Logan Male Senior Baltimore"
-    puts "Chase Male Adult Parkville"
+    # puts "Logan Male Senior Baltimore"
+    # puts "Chase Male Adult Parkville"
+    iggys = []
 
     adoptable_ig = self.new
     adoptable_ig.name = "Logan"
     adoptable_ig.sex = "Male"
     adoptable_ig.location = "Baltimore"
-    adoptable_ig.url = "url"
+    adoptable_ig.url = "logan.com"
+    iggys << adoptable_ig
+
+    adoptable_ig2 = self.new
+    adoptable_ig2.name = "Chase"
+    adoptable_ig2.sex = "Male"
+    adoptable_ig2.location = "Parkville"
+    adoptable_ig2.url = "chase.com"
+    iggys << adoptable_ig2
+    
+    iggys
   end
 end
