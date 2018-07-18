88bda621ff0f420f396a102d72646c77b87128a7
Mark Lawrence <lawrence.ml@gmail.com>
2018-07-17 22:53:17 -0400

    Stubbed out CLI interface

---
+++ NOTES.md
@@ -0,0 +1,11 @@
+What is an adoptable dog?
+
+A dog has a name
+
+A dog has a sex
+
+A dog may have an age
+
+A dog has a location
+
+A dog has a URL
---
+++ lib/adopt_a_dog/cli.rb
@@ -0,0 +1,42 @@
+# CLI Controller
+
+class AdoptADog:: CLI
+
+  def call
+    puts "Welcome to Adopt a Dog!"
+    puts "Enter zip code you want to search for dogs"
+    # input = gets.chomp
+    list_dogs
+    menu
+    goodbye
+  end
+
+  def list_dogs
+    #get adoptable dogs
+    puts "Logan"
+    puts "Chase"
+  end
+
+  def menu
+    input = nil
+
+    while input != "exit"
+      puts "Enter the number of the dog you'd like more info on or type list to see list again or type exit:"
+      input = gets.chomp.downcase
+      case input
+      when "1"
+        puts "More info on Dog Name 1..."
+      when "2"
+        puts "More info on Dog Name 2..."
+      when "list"
+        list_dogs
+      else
+        puts "Not sure? Type list or exit"
+      end
+    end
+  end
+
+  def goodbye
+    puts "Thank you for looking for an adoptable dog."
+  end
+end
