66423eb2c4246035b439132027c54dd21dd7f30d
Mark Lawrence <lawrence.ml@gmail.com>
2018-07-21 00:40:00 -0400

    removed old commented out code since new code for scraping and returning objects and attributes tested successfully

--- lib/adopt_a_dog/cli.rb
+++ lib/adopt_a_dog/cli.rb
@@ -1,6 +1,5 @@
 class AdoptADog::CLI
-
-
+  
   def call
     puts "Welcome to Adopt a Dog!"
     puts "Enter zip code you want to search for dogs"
@@ -34,26 +33,5 @@ class AdoptADog::CLI
 
     main_menu
   end
-
-  # def list_dogs
-  #   @dogs = AdoptADog::Scraper.scrape_dogs("https://www.petsmartcharities.org/find-a-pet-results?city_or_zip=21234&species=dog&color_id&geo_range=50&pet_size_range_id&sex&age=&breed_id=69")
-  #
-  #   @dogs.each.with_index(1) do |dog, i|
-  #     puts "#{i}. #{dog[:name].upcase}, a #{dog[:sex]} #{dog[:breed]} in #{dog[:location]}"
-  #   end
-  #
-  # end
-  #
-  # def add_about_info
-  #   url = @dogs[:url]
-  #   Dogs.all.each do |dog|
-  #     info = AdoptADog::Scraper.scrape_profile_page(url)
-  #     dog.Dogs.add_info(info)
-  #     puts info
-  #   end
-  # end
-
-
-
 
 end
--- lib/adopt_a_dog/scraper.rb
+++ lib/adopt_a_dog/scraper.rb
@@ -2,39 +2,28 @@ class AdoptADog::Scraper
 
   BASE_URL = "https://www.petsmartcharities.org/find-a-pet-results?city_or_zip=21234&species=dog&color_id&geo_range=50&pet_size_range_id&sex&age=&breed_id=69"
 
-  # BASE_PROFILE_URL = "https://www.petsmartcharities.org"
-
   def self.scrape_dogs
     html = open(BASE_URL)
     doc = Nokogiri::HTML(html)
 
-
     doc.css(".pet-result").each do |dog|
-
-
       name = dog.css(".pet-name").text
       breed = dog.css(".pet-breed").text
       sex = dog.css(".pet-sex").text
       location = dog.css(".pet-addr-city-state").text
       url = dog.css("a").attribute("href").value
 
-      # binding.pry
       AdoptADog::Dogs.new(name, breed, sex, location, url)
     end
 
   end
 
   def self.scrape_profile_page(dog)
-    # profile: profile.css(".pet-description").text
-    # shelter name: profile.css(".pet-adoption-info").css(".shelter-name").text
-    # website: profile.css(".pet-adoption-info").css(".website").text
-
     doc = Nokogiri::HTML(open("https://www.petsmartcharities.org#{dog.url}"))
-    binding.pry
+
     dog.story = doc.css(".pet-description").text
     dog.shelter = doc.css(".pet-adoption-info").css(".shelter-name").text
     dog.website = doc.css(".pet-adoption-info").css(".website").text
-    # binding.pry
   end
 
 end
