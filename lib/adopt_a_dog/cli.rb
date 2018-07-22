a060b1b6c799f642499709866bef225dc06f7f41
Mark Lawrence <lawrence.ml@gmail.com>
2018-07-22 01:01:50 -0400

    added menu method for fully working CLI. deleted duplicate file.

--- lib/adopt_a_dog/cli.rb
+++ lib/adopt_a_dog/cli.rb
@@ -3,36 +3,50 @@ class AdoptADog::CLI
 
   def call
     puts "Welcome to Adopt an Italian Greyhound!"
-    puts "Enter zip code"
+    puts "Please enter your zip code"
     zipcode_input = gets.chomp
     AdoptADog::Scraper.scrape_dogs(zipcode_input)
     list_dogs
-    more_info
+    menu
+    goodbye
   end
 
   def list_dogs
-    puts "These dogs are available for adoption in the Baltimore area:"
+    puts "These dogs are available for adoption in your area:"
     puts
     dogs = AdoptADog::Dogs.all
 
     dogs.each.with_index(1) do |dog, i|
         puts "#{i}. #{dog.name.upcase}, a #{dog.sex} #{dog.breed} in #{dog.location}"
     end
+    puts "\nTo view an individual dog's story, select 1-9."
   end
 
-  def more_info
-    puts
-    puts "To view an individual dog's story, select 1-9"
-    input = gets.chomp
-    index = input.to_i - 1
+  def menu
+    input = nil
+      while input != "exit"
+        puts "To see a list of adoptable dogs again, type list.\nTo exit type exit."
+        input = gets.chomp.downcase
 
-    dog = AdoptADog::Dogs.all[index]
-    AdoptADog::Scraper.scrape_profile_page(dog)
+        if input.to_i > 0
+          index = input.to_i - 1
+          dog = AdoptADog::Dogs.all[index]
+          AdoptADog::Scraper.scrape_profile_page(dog)
 
-    puts
-    puts dog.story.gsub(/^\s*/, "")
-    puts
-    puts "To adopt #{dog.name}, please contact #{dog.shelter} at #{dog.website}"
+          puts
+          puts dog.story.gsub(/^\s*/, "")
+          puts
+          puts "To adopt #{dog.name}, please contact #{dog.shelter} at #{dog.website}"
+        elsif input == "list"
+          list_dogs
+        else
+          puts "Try again... Type list or exit."
+        end
+      end
+  end
+
+  def goodbye
+    puts "Thank you for looking at adoptable dogs."
   end
 
 end
